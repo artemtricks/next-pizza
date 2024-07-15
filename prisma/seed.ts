cf05b49b9e908eaa932cd6e0e49e029a2fae9993
Artem <artemtricks@yandex.ru>
2024-07-15 18:45:47 +0700

    added seed wip

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "UserArtaaaa",
+        email: "user@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "adminadmin",
+        email: "admin@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
