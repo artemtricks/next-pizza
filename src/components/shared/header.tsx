ed3d79f104741226aed29d3cd725c5a2db8b750c
Artem <artemtricks@yandex.ru>
2024-07-16 19:14:27 +0700

    added ceeding

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,9 +1,11 @@
 import { cn } from "@/lib/utils";
 import React from "react";
-import { Container } from "./container";
+
 import Image from "next/image";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+import { SearchInput, Container } from "../shared";
 
 type Props = {
   className?: string;
@@ -14,14 +16,19 @@ export const Header = (props: Props) => {
   return (
     <header className={cn("border, border-b ", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">
-              вкусней уже некуда
-            </p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">
+                вкусней уже некуда
+              </p>
+            </div>
           </div>
+        </Link>
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
         <div className="flex items-center gap-3">
           <Button variant="outline" className="flex items-center gap-1">
---
+++ src/components/shared/search-input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {
+  className?: string;
+};
+
+export const SearchInput = (props: Props) => {
+  const { className } = props;
+  return <div className={className}>inputinputinputinputinputinputinput</div>;
+};
